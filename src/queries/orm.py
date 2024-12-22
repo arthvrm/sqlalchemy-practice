c6f510c6ee82cc1eef9567f2b27b130e540d088a
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-23 00:08:51 +0200

    replaced metadata by using ORM-based solution(by Base)

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,13 +1,13 @@
 from sqlalchemy import text, insert
 from database import sync_engine, async_engine, session_factory, async_session_factory
-from models import metadata_obj, WorkersOrm
+from models import WorkersOrm, Base
 
 
 
 def create_tables() -> None:
-    sync_engine.echo = False             # виключення виведення логів у консоль
-    metadata_obj.drop_all(sync_engine)   # видаляє всі таблиці з бд на основі підключення до бд
-    metadata_obj.create_all(sync_engine) # створює все що знаходиться в об'єкті метадати, на основі двигуна що з'єднується з бд
+    #sync_engine.echo = False             # виключення виведення логів у консоль
+    Base.metadata.drop_all(sync_engine)   # видаляє всі таблиці з бд на основі підключення до бд
+    Base.metadata.create_all(sync_engine) # створює все що знаходиться в об'єкті метадати, на основі двигуна що з'єднується з бд
     sync_engine.echo = True              # включення виведення логів у консоль
 
 
