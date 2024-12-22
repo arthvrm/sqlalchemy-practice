2a89373dff593e2c683d202fe1ca3375d9d533a2
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-22 03:35:34 +0200

    added declarative style (for orm)

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData
+from database import Base
+from sqlalchemy.orm import Mapped, mapped_column
 
 
+"""імперативний стиль"""
 metadata_obj = MetaData() # об'єкт що передається у всі теблиці/моделі створені на стороні програми щоб мати доступ для взаємодії з ними(таблицями/моделями)
 
 
@@ -10,3 +13,11 @@ workers_table = Table(                       # створення таблиці
     Column("id", Integer, primary_key=True), # створення стовпця name, type, pk
     Column("username", String),
 )
+
+
+"""декларативний стиль"""
+class WorkersOrm(Base):
+    __tablename__ = "workers"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]# = mapped_column()
