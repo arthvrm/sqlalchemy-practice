17317480fbcd625408301e1ba43b642a58225a96
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-23 00:06:10 +0200

    added Annotations, workload(actually enum) type for db and Resumes table-model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,19 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
-from database import Base
+import datetime
+from typing import Optional, Annotated
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, func, text
+from database import Base, str_255
 from sqlalchemy.orm import Mapped, mapped_column
+import enum
+
+"""annotations below"""
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+updated_at = Annotated[datetime.datetime, mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow
+    )]
+
 
 
 """імперативний стиль"""
@@ -19,5 +32,22 @@ workers_table = Table(                       # створення таблиці
 class WorkersOrm(Base):
     __tablename__ = "workers"
     
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[intpk]# = mapped_column(primary_key=True)
     username: Mapped[str]# = mapped_column()
+
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+
+class Resumes(Base):
+    __tablename__ = "resumes"
+    
+    id: Mapped[intpk]# = mapped_column(primary_key=True)
+    title: Mapped[str_255]# = mapped_column(String(200))
+    compensation: Mapped[Optional[int]] # Mapped[int | None] # Mapped[int] = mapped_column(nullable=True)
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE")) # ForeignKey(WorkersOrm.id) <- usualy not in usage
+    created_at: Mapped[created_at]# = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[updated_at]# = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=datetime.datetime.utcnow)
