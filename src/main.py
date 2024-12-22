460f62230e1ee360583c9ebde058406982939332
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-22 03:38:24 +0200

    updated

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
+import asyncio
 import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
-from queries.core import create_tables, insert_data
+# from queries.core import create_tables, insert_data, get_123_sync
+from queries.orm import create_tables, insert_data
 
-
+"""це основний файл з якого будем запускати аплікуху"""
 create_tables()
 insert_data()
-
-
+# asyncio.run(insert_data())
