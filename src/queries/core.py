4bea2049141957849df3955549da15eb4bad3e05
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-23 04:55:38 +0200

    added generalizing classes Sync and Async, added select and update methods and their async versions

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select, update
 from database import sync_engine, async_engine
 from models import metadata_obj, workers_table
 
@@ -15,23 +15,86 @@ async def get_123_async() -> None:
         print(f"{res.all()[0]=}")
 
 
-def create_tables() -> None:
-    sync_engine.echo = False             # виключення виведення логів у консоль
-    metadata_obj.drop_all(sync_engine)   # видаляє всі таблиці з бд на основі підключення до бд
-    metadata_obj.create_all(sync_engine) # створює все що знаходиться в об'єкті метадати, на основі двигуна що з'єднується з бд
-    sync_engine.echo = True              # включення виведення логів у консоль
+class SyncCore:
+    @staticmethod                            # декоратор що перетворює функцію в статичний метод -> означає що для виклику методу не треба створювати екземпляр на основі класу
+    def create_tables() -> None:
+        sync_engine.echo = False             # виключення виведення логів у консоль
+        metadata_obj.drop_all(sync_engine)   # видаляє всі таблиці з бд на основі підключення до бд
+        metadata_obj.create_all(sync_engine) # створює все що знаходиться в об'єкті метадати, на основі двигуна що з'єднується з бд
+        sync_engine.echo = True              # включення виведення логів у консоль
+
+    @staticmethod
+    def insert_workers() -> None:
+        with sync_engine.connect() as conn:
+            # text(stmt = """INSERT INTO workers (username) VALUES # швидший за часом спосіб виконання sql-script'а хоча пишеться вручну
+            #     ('Beaver'), 
+            #     ('Wolf');""")
+            stmt = insert(workers_table).values(                   # instrument: "query builder", довший за часом спосіб виконання sql-script'а зате пишеться синтаксисом пайтона
+                [
+                    {"username": "Beaver"},
+                    {"username": "Wolf"},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()                                          # оскільки .connect() -> return ROLLBACK, треба вручну робити COMMIT(щоб дані записались в таблицю)
+
+    @staticmethod
+    def select_workers() -> None:
+        with sync_engine.connect() as conn:
+            query = select(workers_table) # SELECT * FROM workers
+            result = conn.execute(query)
+            workers = result.all()
+            print(f"{workers=}")
+
+    @staticmethod
+    def update_worker(worker_id: int = 2, new_username: str = "Squirrel") -> None:
+        with sync_engine.connect() as conn:
+            # stmt = text("UPDATE workers SET username=:username WHERE id=:id") # для захисту від SQL-ін'єкцій використовуємо НЕ f-strings/format()
+            # stmt = stmt.bindparams(username=new_username, id=worker_id)       # а вбудовану в підкапот(psycopg2||3) функцію bindparams()
+            stmt = (
+                update(workers_table)
+                .values(username=new_username)
+                # .where(workers_table.c.id==worker_id) # нижче .filter_by наслідується від .where тому сильної різнці між цими двома нема,
+                .filter_by(id=worker_id)                # хіба що різниться синтаксис запису аргументів
+            )
+            conn.execute(stmt)
+            conn.commit()
 
 
-def insert_data() -> None:
-    with sync_engine.connect() as conn:
-        # text(stmt = """INSERT INTO workers (username) VALUES # швидший за часом спосіб виконання sql-script'а хоча пишеться вручну
-        #     ('Beaver'), 
-        #     ('Wolf');""")
-        stmt = insert(workers_table).values(                   # instrument: "query builder", довший за часом спосіб виконання sql-script'а зате пишеться синтаксисом пайтона
-            [
-                {"username": "Beaver"},
-                {"username": "Wolf"},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()                                          # оскільки .connect() -> return ROLLBACK, треба вручну робити COMMIT(щоб дані записались в таблицю)
+class AsyncCore:
+    @staticmethod
+    async def create_tables() -> None:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(metadata_obj.drop_all)
+            await conn.run_sync(metadata_obj.create_all)
+
+    @staticmethod
+    async def insert_workers() -> None:
+        async with async_engine.connect() as conn:
+            stmt = insert(workers_table).values(
+                [
+                    {"username": "Beaver"},
+                    {"username": "Wolf"},
+                ]
+            )
+            await conn.execute(stmt)
+            await conn.commit()
+    
+    @staticmethod
+    async def select_workers() -> None:
+        async with async_engine.connect() as conn:
+            query = select(workers_table)
+            result = await conn.execute(query)
+            workers = result.all()
+            print(f"{workers=}")
+    
+    @staticmethod
+    async def update_worker(worker_id: int = 2, new_username: str = "Squirrel") -> None:
+        async with async_engine.connect() as conn:
+            stmt = (
+                update(workers_table)
+                .values(username=new_username)
+                .filter_by(id=worker_id)
+            )
+            await conn.execute(stmt)
+            await conn.commit()
