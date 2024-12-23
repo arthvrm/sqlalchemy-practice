bd19582dc79907471ef4f6012bd9487c88eec5dc
arthvrm <153835086+arthvrm@users.noreply.github.com>
2024-12-23 06:26:14 +0200

    updated

--- src/main.py
+++ src/main.py
@@ -3,9 +3,39 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 # from queries.core import create_tables, insert_data, get_123_sync
-from queries.orm import create_tables, insert_data
+from queries.orm import SyncORM, AsyncORM
+from queries.core import SyncCore, AsyncCore
 
-"""це основний файл з якого будем запускати аплікуху"""
-create_tables()
+# create_tables()
 # insert_data()
 # asyncio.run(insert_data())
+
+SyncORM.create_tables()
+# SyncCore.create_tables()
+
+SyncORM.insert_workers()
+# SyncCore.insert_workers()
+
+SyncORM.update_worker()
+# SyncCore.update_worker()
+
+SyncORM.select_workers()
+# SyncCore.select_workers()
+
+
+
+# async def main():
+#     await AsyncORM.create_tables()
+#     # await AsyncCore.create_tables()
+    
+#     # await AsyncORM.insert_workers()
+#     await AsyncCore.insert_workers()
+    
+#     # await AsyncORM.update_worker()
+#     await AsyncCore.update_worker()
+
+#     # await AsyncORM.select_workers()
+#     await AsyncCore.select_workers()
+
+# # Execute the main function
+# asyncio.run(main())
